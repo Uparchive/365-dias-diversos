9674008c31ad473dcbea2f879b01378cace6fd17
Uparchive <140474627+Uparchive@users.noreply.github.com>
2024-11-15 04:33:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let currentPage = 0;
 
     // Recupera o último capítulo lido do localStorage
-    const lastReadChapter = localStorage.getItem("365-dias-diversos");
+    const lastReadChapter = localStorage.getItem("diariodeomega");
     if (lastReadChapter && !isNaN(parseInt(lastReadChapter))) {
         currentPage = parseInt(lastReadChapter);
         // Aplica a classe 'flip' nas páginas anteriores ao último capítulo lido
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Função para atualizar o último capítulo lido no localStorage
     function setLastReadChapter(chapterIndex) {
-        localStorage.setItem("365-dias-diversos", chapterIndex);
+        localStorage.setItem("diariodeomega", chapterIndex);
     }
 
     // Função para atualizar o destaque no capítulo atual
